b2919777230043804f93b2643bccb474c70c8f2f
paulbknhs <dev@paulbknhs.de>
2025-04-10 15:34:01 +0200

    finished counter method

--- Assignment_01/IntList.java
+++ Assignment_01/IntList.java
@@ -11,17 +11,33 @@ public class IntList {
     System.out.println(test3);
     System.out.println(countNumbers(test3));
 
+    intToList(999);
   }
 
   public static int countNumbers(int num) {
-    int count = 0;
+    int count = 1;
     while ((int) (num / 10) > 0) {
       count++;
-      System.out.println(num);
+      // System.out.println(num);
       num = (int) num / 10;
-      System.out.println(num);
+      // System.out.println(num);
     }
     return count;
+  }
+
+  public static int[] intToList(int value) {
+    int[] valueArray = new int[countNumbers(value)];
+    for (int i = 0; i < countNumbers(value); i++) {
+      if (value > 9) {
+        valueArray[i] = value % 10;
+        value = (int) value / 10;
+      } else {
+        valueArray[i] = value;
+      }
+      System.out.print("[" + valueArray[i] + "]");
+    }
+    System.out.println("");
+    return valueArray;
   }
 
 }
