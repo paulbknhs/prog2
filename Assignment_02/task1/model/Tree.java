d1a9f9279c88a424b52ebf8c6c7b891fc991b116
paulbknhs <dev@paulbknhs.de>
2025-04-20 21:03:53 +0200

    only missing testmethods

--- Assignment_02/task1/model/Tree.java
+++ Assignment_02/task1/model/Tree.java
@@ -77,12 +77,71 @@ public class Tree {
   }
 
   public void deleteValue(int value) {
-    List<Integer> treeList = new ArrayList<>();
-    traverse(this, treeList);
-    treeList.remove(Integer.valueOf(value));
-    Tree tmp = new Tree(this.value);
-    for (Integer val : treeList) {
-      tmp.insertValue(val);
+    if (value == this.value) {
+      if (this.left == null && this.right == null) {
+        return;
+      }
+      
+      if (this.left == null) {
+        this.value = this.right.value;
+        this.left = this.right.left;
+        this.right = this.right.right;
+      } else if (this.right == null) {
+        this.value = this.left.value;
+        this.right = this.left.right;
+        this.left = this.left.left;
+      } else {
+        Tree successor = findMin(this.right);
+        this.value = successor.value;
+        deleteSuccessor(this.right, successor.value);
+      }
+    } else if (value < this.value && this.left != null) {
+      this.left.deleteValue(value);
+    } else if (value > this.value && this.right != null) {
+      this.right.deleteValue(value);
     }
+  }
+
+  private void deleteSuccessor(Tree node, int value) {
+    if (node.left == null && node.right == null) {
+      return;
+    }
+    
+    if (node.left != null && node.left.value == value) {
+      if (node.left.left == null && node.left.right == null) {
+        node.left = null;
+      } else if (node.left.left == null) {
+        node.left = node.left.right;
+      } else if (node.left.right == null) {
+        node.left = node.left.left;
+      } else {
+        Tree successor = findMin(node.left.right);
+        node.left.value = successor.value;
+        deleteSuccessor(node.left.right, successor.value);
+      }
+    } else if (node.right != null && node.right.value == value) {
+      if (node.right.left == null && node.right.right == null) {
+        node.right = null;
+      } else if (node.right.left == null) {
+        node.right = node.right.right;
+      } else if (node.right.right == null) {
+        node.right = node.right.left;
+      } else {
+        Tree successor = findMin(node.right.right);
+        node.right.value = successor.value;
+        deleteSuccessor(node.right.right, successor.value);
+      }
+    } else if (value < node.value) {
+      deleteSuccessor(node.left, value);
+    } else {
+      deleteSuccessor(node.right, value);
+    }
+  }
+
+  private Tree findMin(Tree node) {
+    while (node.left != null) {
+      node = node.left;
+    }
+    return node;
   }
 }
