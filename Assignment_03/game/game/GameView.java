d3da7b763da227a5a4e81f42a1374c98b307869c
paulbknhs <dev@paulbknhs.de>
2025-04-24 17:44:57 +0200

    added str

--- Assignment_03/game/game/GameView.java
+++ Assignment_03/game/game/GameView.java
@@ -1,6 +1,7 @@
 package game;
 
-import game.GameObject;
+import java.lang.StringBuilder;
+import GameObject;
 import entity.NPC;
 
 public class GameView {
@@ -8,5 +9,20 @@ public class GameView {
 
   public GameView() {
     this.view = new GameObjects[20][3];
+  }
+
+  public str() {
+    StringBuilder sb = new StringBuilder();
+    for (int i = 0; i < view.length; i++) {
+      for (int j = 0; j < view[i].length; j++) {
+        if (view[i][j] != null) {
+          sb.append(view[i][j].toString());
+        } else {
+          sb.append(" ");
+        }
+      }
+      sb.append("\n");
+    }
+    return sb.toString();
   }
 }
