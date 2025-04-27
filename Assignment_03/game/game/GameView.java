0e70d343b6d92d8ac2c40ca54f38df14fd010abf
paulbknhs <dev@paulbknhs.de>
2025-04-27 23:25:29 +0200

    added gameview

--- Assignment_03/game/game/GameView.java
+++ Assignment_03/game/game/GameView.java
@@ -1,28 +1,50 @@
 package game;
 
+import java.util.ArrayList;
 import java.lang.StringBuilder;
+import java.util.Arrays;
+import java.util.List;
+
+import GameObjects.GameObject;
 import game.GameObjects;
 import entity.NPC;
 
 public class GameView {
-  GameObjects[][] view;
+
+  GameObjects.GameObject[][] view;
 
   public GameView() {
-    this.view = new GameObjects[20][3];
+    this.view = new GameObjects.GameObject[20][3];
+    NPC player = new NPC();
+    List<Integer> lavaTilesX = Arrays.asList(4, 8, 12, 16);
+    List<Integer> obstacleTilesX = Arrays.asList(2, 6, 10, 14, 18);
+    int goalX = 19;
+
+    for (int x = 0; x < 20; x++) {
+      for (int y = 0; y < 3; y++) {
+        switch (y) {
+          case 0:
+            if (lavaTilesX.contains(x)) {
+              this.view[x][y] = GameObjects.GameObject.LAVA;
+            } else if (x == 19) {
+              this.view[x][y] = GameObjects.GameObject.GOAL;
+            } else {
+              this.view[x][y] = GameObjects.GameObject.GROUND;
+            }
+          case 1:
+            this.view[x][y] = GameObjects.GameObject.EMPTY;
+          case 2:
+            if (obstacleTilesX.contains(x)) {
+              this.view[x][y] = GameObjects.GameObject.AIROBSTACLE;
+            } else {
+              this.view[x][y] = GameObjects.GameObject.EMPTY;
+            }
+        }
+      }
+    }
   }
 
   public String str() {
-    StringBuilder sb = new StringBuilder();
-    for (int i = 0; i < view.length; i++) {
-      for (int j = 0; j < view[i].length; j++) {
-        if (view[i][j] != null) {
-          sb.append(view[i][j].toString());
-        } else {
-          sb.append(" ");
-        }
-      }
-      sb.append("\n");
-    }
-    return sb.toString();
+
   }
 }
