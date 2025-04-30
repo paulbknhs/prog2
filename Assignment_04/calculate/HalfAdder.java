5519c60e2004a33a154db85d3347c7e5905cb01d
paulbknhs <dev@paulbknhs.de>
2025-04-30 23:25:25 +0200

    added halfadder class

---
+++ Assignment_04/calculate/HalfAdder.java
@@ -0,0 +1,16 @@
+package calculate;
+import logic.*;
+
+public class HalfAdder {
+    Gate[] gates;
+
+    public HalfAdder() {
+        this.gates = new Gate[]{new XorGate(), new AndGate()};
+    }
+
+    public boolean[] add(boolean a, boolean b) {
+        boolean c = gates[1].evaluate(a, b);
+        boolean s = gates[0].evaluate(a, b);
+        return new boolean[]{s, c};
+    }
+}
